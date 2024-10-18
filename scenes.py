24526734d9000b340b14820cc4cd35d54cb8a91f
residualentropy <161914158+residualentropy@users.noreply.github.com>
2024-10-17 19:59:09 -0500

    table

--- scenes.py
+++ scenes.py
@@ -2,5 +2,13 @@ import manim as m
 
 class Eqn(m.Scene):
     def construct(self):
-        eqn = m.Tex(r'$E \neq mc^2$', font_size= 60)
-        self.play(m.Write(eqn))
+        with_ce = m.TexTemplate()
+        with_ce.add_to_preamble(r"\usepackage[version=4]{mhchem}")
+        table = m.Table(
+            [["This", "is a"],
+            ["simple", "Table."]],
+            row_labels= [m.Tex("Shielded"), m.Tex("Unshielded")],
+            col_labels= [m.Tex(r"From \ce{^241Am}", tex_template= with_ce), m.Tex("Background")],
+        )
+        self.play(m.Create(table))
+        self.wait(5)
